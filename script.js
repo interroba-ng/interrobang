a2bdc802c7f5742c8aaa886c97d1644c884b0432
interroba-ng <keefy.bby@gmail.com>
2024-12-01 00:37:47 -0700

    .

--- script.js
+++ script.js
@@ -31,11 +31,7 @@ const commands = [
       description: 'owo',
       execute: () => "owo",
     },
-    {
-      name: 'gravity-falls',
-     description: 'my little sisters favorite show',
-      execute: () => "dipper and mabel go on a trip and find bill randomly, and ford beats bill.",
-    }
+
   ];
   
   // Terminal initialization
